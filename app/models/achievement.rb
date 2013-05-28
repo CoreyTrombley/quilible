9b2ec7b33b192684d0ad7a7e006c33f7529be1d8
Corey Trombley <corey.trombley33@gmail.com>
2013-05-27 23:21:28 -0400

    starting over

--- app/models/achievement.rb
+++
@@ -1,3 +0,0 @@
-class Achievement < ActiveRecord::Base
-  attr_accessible :description, :points, :title
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Quilible::Application.config.secret_token = 'c36c2b9569019bf5a6366f29cf107527016a61871da2b415a98bd47c5472db56c630ad59db562ae9921e99fded0f8d8ba0bb5dfbf571c669dafaee1623249544'
