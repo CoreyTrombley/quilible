2e2ab5fede001bd32b1b0fb2fe53f881b003ae10
Corey Trombley <corey.trombley33@gmail.com>
2013-05-27 23:25:19 -0400

    re init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quilible::Application.config.secret_token = '1115c67b321d71b81332d79e8134b497f1b4e43ab07d0fc94e682e03a4c241ec5eaaa9129ec7fe61c612da5423ce502dd154525c58433fc0a438f1021d0b9603'
