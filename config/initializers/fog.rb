0f9224642467c4da8539d206132a860462d96012
Corey Trombley <corey.trombley33@gmail.com>
2013-06-12 15:29:13 -0400

    fixed error

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -3,9 +3,9 @@ Fog.credentials_path = Rails.root.join('config/fog_credentials.yml')
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider => 'AWS',
-    :aws_access_key_id      => 'AKIAICBFHQAOC6XMQDDQ',                        # required
-    :aws_secret_access_key  => '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',                        # required
-    :region                 => 'us-east-1',                  # optional, defaults to 'us-east-1'
+    :aws_access_key_id      => 'AKIAICBFHQAOC6XMQDDQ', # required
+    :aws_secret_access_key  => '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', # required
+    :region                 => 'us-east-1' # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = "Quilible" # required
   config.fog_public     = true                                   # optional, defaults to true
