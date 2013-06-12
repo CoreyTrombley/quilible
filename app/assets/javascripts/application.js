16202eeca877fb84123dc0c839a716d3a2f0cf9d
Corey Trombley <corey.trombley33@gmail.com>
2013-06-12 17:11:42 -0400

    testing camera permissions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,32 @@
 //= require_tree .
 
 $(document).foundation();
+
+
+
+window.URL = window.URL || window.webkitURL;
+navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+var video = document.querySelector('video');
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+var localMediaStream = null;
+
+function snapshot() {
+  if (localMediaStream) {
+    ctx.drawImage(video, 0, 0);
+    // "image/webp" works in Chrome 18. In other browsers, this will fall back to image/png.
+    document.querySelector('img').src = canvas.toDataURL('image/webp');
+  }
+}
+
+video.addEventListener('click', snapshot, false);
+
+
+if (navigator.getUserMedia) {
+  navigator.getUserMedia({audio: true, video: true}, function(stream) {
+    video.src = window.URL.createObjectURL(stream);
+  });
+} else {
+  video.src = 'somevideo.webm'; // fallback.
+}
